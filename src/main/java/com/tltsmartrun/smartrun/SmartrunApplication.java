1c8c5d81e91e9517da0172fa5de8130473f205ba
Mokgoatjane, Tshepo <tshepo.mokgoatjane@fnb.co.za>
2024-03-05 20:23:49 +0200

    Initial commit

---
+++ src/main/java/com/tltsmartrun/smartrun/SmartrunApplication.java
@@ -0,0 +1,13 @@
+package com.tltsmartrun.smartrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartrunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartrunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tltsmartrun/smartrun/SmartrunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tltsmartrun.smartrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartrunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
